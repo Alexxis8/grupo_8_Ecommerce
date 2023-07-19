cfd378225e6971304bd832f49abf35ad42c0bb76
tincho282 <tincho282@hotmail.com>
2023-07-19 09:58:56 -0300

    Cambios app.js

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ app.use(express.static('public'))
 app.listen(3000,()=> console.log('Servidor funcionando en http://localhost:3000'))
 
 app.get('/', (req,res)=> {
-        res.sendFile(path.join(__dirname,'/html/index.html'))
+        res.sendFile(path.join(__dirname,'/views/index.html'))
+} )
+app.get('/login', (req,res)=> {
+        res.sendFile(path.join(__dirname,'/views/login.html'))
+} )
+app.get('/register', (req,res)=> {
+        res.sendFile(path.join(__dirname,'/views/register.html'))
+} )
+app.get('/productDetail', (req,res)=> {
+        res.sendFile(path.join(__dirname,'/views/productDetail.html'))
+} )
+app.get('/productCart', (req,res)=> {
+        res.sendFile(path.join(__dirname,'/views/productCart.html'))
 } )
