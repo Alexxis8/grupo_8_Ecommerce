6f6e2d0a5bcd715779f2e9bca04a79def0330de1
Alexxis8 <yo.alexis08@gmail.com>
2023-07-22 21:27:15 -0300

    modificando index

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-app.use(express.static('public'))
-app.listen(3000,()=> console.log('Servidor funcionando en http://localhost:3000'))
+app.use(express.static('public'));
+app.listen(3000, ()=> console.log('Servidor funcionando en http://localhost:3000'));
 
 app.get('/', (req,res)=> {
         res.sendFile(path.join(__dirname,'/views/index.html'))
