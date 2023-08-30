e5421440669be6860fa6b21e8895f98c56bdfedc
Alexxis8 <yo.alexis08@gmail.com>
2023-08-29 21:11:22 -0300

    se crearon archivos ejs y carpeta partials

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,19 @@
 const controller = {
-
+    index:(req, res)=>{
+        res.render('index')
+    },
+    login:(req, res)=>{
+        res.render('login')
+    },
+    carrito:(req, res)=>{
+        res.render('productCart')
+    },
+    detalle:(req, res)=>{
+        res.render('productDetail')
+    },
+    register:(req, res)=>{
+        res.render('register')
+    }
 }
 
 module.exports = controller
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const controller = require('../controllers/mainController')
 const router = express.Router()
 
+
+router.get('/', controller.index)
+router.get('/login', controller.login)
+router.get('/productCart', controller.carrito)
+router.get('/productDetail', controller.detalle)
+router.get('/register', controller.register)
 
 module.exports = router
