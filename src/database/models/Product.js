50887f29505d132368cb1fa80cd2342829d00787
alexxis8 <yo.alexis08@gmail.com>
2023-10-26 18:33:22 -0300

    modificando modelos

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -1,6 +1,39 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = 'Product'
-    let cols = {}
+    let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        cilindrada:{
+            type: dataTypes.DECIMAL(8,2)
+        },
+        modelo:{
+            type: dataTypes.STRING(255)
+        },
+        estilo:{
+            type: dataTypes.INTEGER
+        },
+        precio:{
+            type: dataTypes.DECIMAL(8,2)
+        },
+        imagen:{
+            type: dataTypes.STRING(255)
+        },
+        stock:{
+            type: dataTypes.INTEGER
+        },
+        descripcion:{
+            type: dataTypes.STRING(255)
+        },
+        color:{
+            type: dataTypes.INTEGER
+        },
+        marca:{
+            type: dataTypes.INTEGER
+        }
+    }
     let config = {
         tableName: 'productos',
         timestamps: false
--- src/database/models/User.js
+++ src/database/models/User.js
@@ -1,6 +1,18 @@
 module.exports = (sequelize, dataTypes)=>{
     let alias = 'User'
-    let cols = {}
+    let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        nombre:{
+            type: dataTypes.STRING(255)
+        },
+        apellido:{
+            type: dataTypes.STRING(255)
+        },
+    }
     let config = {
         tableName: 'usuarios',
         timestamps: false
